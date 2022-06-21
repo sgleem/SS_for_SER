7ffec79a089c9f345df59f7a982b2f536f931976
sgleem <sxl190087@utdallas.edu>
2022-06-21 09:46:49 -0500

    0620-change-run

--- run.sh
+++ run.sh
@@ -1,42 +1,56 @@
 #!/bin/bash
 
 ## Training
-python -u train.py \
+# python -u train.py \
+# --device            cuda \
+# --data_type         clean \
+# --model_type        wav2vec2 \
+# --conf_path         conf.json \
+# --seed              0 \
+# --batch_size        16 \
+# --hidden_dim        1024 \
+# --num_layers        2 \
+# --epochs            10 \
+# --lr                1e-4 \
+# --noise_dur         30m \
+# --model_path        model/wav2vec2/1_8/clean_0
+
+# ## Evaluation
+python -u test.py \
 --device            cuda \
 --data_type         clean \
 --model_type        wav2vec2 \
+--train_type        manually_finetuned \
 --conf_path         conf.json \
 --seed              0 \
---batch_size        16 \
+--batch_size        1 \
 --hidden_dim        1024 \
 --num_layers        2 \
---epochs            10 \
---lr                1e-4 \
---noise_dur         30m \
 --model_path        model/wav2vec2/1_8/clean_0
 
-# ## Evaluation
-# python -u test.py \
-# --device            cuda \
-# --data_type         clean \
-# --model_type        wavlm \
-# --train_type        manually_finetuned \
-# --conf_path         conf.json \
-# --seed              0 \
-# --batch_size        1 \
-# --hidden_dim        1024 \
-# --num_layers        2 \
-# --model_path        model/wavlm/1_10/clean_0
-
-python -u retrain.py \
+python -u test.py \
 --device            cuda \
---data_type         manual_noisy_speech \
---feature_type      wav2vec \
+--data_type         noisy \
+--snr               10db \
+--model_type        wav2vec2 \
+--train_type        manually_finetuned \
+--conf_path         conf.json \
 --seed              0 \
---batch_size        16 \
+--batch_size        1 \
 --hidden_dim        1024 \
 --num_layers        2 \
---epochs            10 \
---lr                1e-4 \
---noise_dur         30m \
---model_path        model/wav2vec_finetune/10db_random_0
+--model_path        model/wav2vec2/1_8/clean_0
+
+# python -u retrain.py \
+# --device            cuda \
+# --data_type         manual_noisy_speech \
+# --model_type      wav2vec2 \
+# --seed              0 \
+# --batch_size        16 \
+# --hidden_dim        1024 \
+# --num_layers        2 \
+# --epochs            10 \
+# --lr                1e-4 \
+# --noise_dur         30m \
+# --original_model_path model/wav2vec2/1_8/clean_0 \
+# --model_path        model/wav2vec2_finetune/1_8/10db_noisysample_0
